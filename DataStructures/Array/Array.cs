ffc47a2371dbcf81516954a699e004ede46d6adb
IsseHassan <cadeysey114@gmail.com>
2023-03-20 21:37:20 +0300

    Week_02

--- DataStructures/Array/Array.cs
+++ DataStructures/Array/Array.cs
@@ -1,6 +1,8 @@
-﻿namespace Array;
+﻿using System.Collections;
 
-public class Array
+namespace Array;
+
+public class Array : IEnumerable
 {
     // Object
     // Type : Array
@@ -14,6 +16,12 @@ public class Array
     public Array()
     {
         _InnerArray = new Object[4]; // Block allocation
+    }
+    public Array(params Object[] init)
+    {
+        var newArray = new Object[init.Length];
+        System.Array.Copy(init, newArray, init.Length);
+        _InnerArray = newArray;
     }
 
     public void Add(Object item)
@@ -36,6 +44,54 @@ public class Array
 
     public Object GetItem(int position)
     {
+        //Exception
         return _InnerArray[position];
     }
+    public void SetItem(int position, Object item)
+    {
+        _InnerArray[position] = item;
+    }
+    /// <summary>
+    /// Week 1 - Implementation 1 
+    /// 
+    /// </summary>
+    /// <param name="position"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void RemoveItem(Object item)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    ///  Week - 1 Implementation 2
+    /// </summary>
+    /// <param name="p1"></param>
+    /// <param name="p2"></param>
+    public void Swap(int p1, int p2)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Week - 1 Implementation 3
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public int Find(Object item)
+    {
+        for (int i = 0; i < _InnerArray.Length; i++)
+        {
+            if (item.Equals(_InnerArray[i]))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return _InnerArray.GetEnumerator();
+    }
+
 }
--- DataStructures/ArrayTests/ArrayTests.cs
+++ DataStructures/ArrayTests/ArrayTests.cs
@@ -52,5 +52,74 @@ namespace ArrayTests
             // Assert
             Assert.Equal(item, "Mehmet");
         }
+        [Fact]
+        public void Arrry_Find_Test()
+        {
+            // Arrange
+            var array = new Array.Array();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Add(4);
+
+            // Act
+            int result = array.Find(2);
+
+            // Assert
+            Assert.Equal(result, 1);
+        }
+
+        [Fact]
+        public void Array_GetEnumerator()
+        {
+            // Arrange
+            var array = new Array.Array();
+            array.Add("Ahmet");
+            array.Add("Mehmet");
+            array.Add("Can");
+
+            string result = "";
+            foreach (var item in array)
+            {
+                result = string.Concat(result, item);
+            }
+
+            Assert.Equal(result, "AhmetMehmetCan");
+
+        }
+
+        [Fact]
+        public void Array_Contructor_Test()
+        {
+            // Arrange
+            var array = new Array.Array(36, 23, 55, 44, 61);
+
+            // Act
+            var result = array.Capacity; // 5
+
+            var result2 = String.Empty;
+            foreach (var item in array)
+            {
+                result2 = string.Concat(result2, item);
+            }
+
+            // Assert
+            Assert.Equal(5, result);
+            Assert.Equal("3623554461", result2);
+        }
+
+        [Fact]
+        public void Array_SetItem_Test()
+        {
+            // Arrange : Düzenleme
+            var numbers = new Array.Array(1, 3, 5, 7);
+
+            // Act : Eylem
+            numbers.SetItem(2, 55);
+
+            // Assert : İddia
+            Assert.Equal(55, numbers.GetItem(2));
+            Assert.True(numbers.GetItem(2).Equals(55));
+        }
     }
 }
