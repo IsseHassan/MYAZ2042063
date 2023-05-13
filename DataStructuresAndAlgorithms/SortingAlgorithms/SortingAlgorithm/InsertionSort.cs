92d76dcb8f3a167561be1ef24c307e9fa53c3891
IsseHassan <cadeysey114@gmail.com>
2023-05-13 22:31:04 +0300

    Sorting algorithms updated

--- DataStructuresAndAlgorithms/SortingAlgorithms/SortingAlgorithm/InsertionSort.cs
+++ DataStructuresAndAlgorithms/SortingAlgorithms/SortingAlgorithm/InsertionSort.cs
@@ -1,25 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace SortingAlgorithm
 {
     public class InsertionSort
     {
-        public enum SortDirection
+        public static T[] Sort<T>(T[] array)
+            where T : IComparable<T>
         {
-            Ascending,
-            Descending
-        }
-
-        public static T[] Sort<T>(T[] array,
-            SortDirection sortDirection = SortDirection.Ascending)
-            where T : IComparable
-        {
-            var comparer = new CustomComparer<T>(sortDirection, Comparer<T>.Default);
-            for (int i = 0; i < array.Length -1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int j = i+1; j < array.Length; j++)
+
+                for (int j = 0; j > 0; j--)
                 {
-                    if (comparer.Compare(array[i], array[j - 1]) < 0)
+                    if (comparer.Compare(array[j], array[j - 1]) < 0)
                     {
                         Sorting.Swap<T>(array, j - 1, j);
                     }
@@ -29,7 +23,10 @@ namespace SortingAlgorithm
                     }
                 }
             }
+
             return array;
         }
+            
+        
     }
 }
